f6c8a4561d8f8f28b3cd6cc9fda7ddba89d38afb
oscarkayaya <oscarkayaya01@gmail.com>
2026-06-23 15:52:49 -0700

    remove filter stream, and fixed the edit products

--- src/main/java/org/yearup/controllers/CategoriesController.java
+++ src/main/java/org/yearup/controllers/CategoriesController.java
@@ -1,40 +1,63 @@
 package org.yearup.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 import org.yearup.models.Category;
 import org.yearup.models.Product;
 import org.yearup.service.CategoryService;
 import org.yearup.service.ProductService;
 
+import java.util.ArrayList;
 import java.util.List;
 
 // add the annotations to make this a REST controller
 // add the annotation to make this controller the endpoint for the following url
     // http://localhost:8080/categories
 // add annotation to allow cross site origin requests
+
+@RestController
+@RequestMapping("/categories")
+@CrossOrigin
+
 public class CategoriesController
 {
     private CategoryService categoryService;
     private ProductService productService;
 
 
-    // create an Autowired constructor to inject the categoryService and productService
-
-    // add the appropriate annotation for a get action
-    public List<Category> getAll()
+    // create an Autowired constructor to inject the categoryService and productService @contract(pure = true)
+    @Autowired
+    public CategoriesController(CategoryService categoryService, ProductService productService)
     {
-        // find and return all categories
-        return null;
+        this.categoryService = categoryService;
+        this.productService = productService;
+
     }
 
     // add the appropriate annotation for a get action
-    public Category getById(@PathVariable int id)
+    @GetMapping
+    public ResponseEntity<List<Category>> getAll()
     {
+        // find and return all categories
+        var categories = categoryService.getAllCategories();
+        if (categories.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        // find and return all categories
+        return ResponseEntity.ok(categories);
+    }
+
+    // add the appropriate annotation for a get action
+    @GetMapping("{id}")
+    public ResponseEntity<Category> getById(@PathVariable int id)
+    {
+        var category = categoryService.getById(id);
+        if(category == null) return ResponseEntity.notFound().build();
         // get the category by id
-        return null;
+        return ResponseEntity.ok(category);
     }
 
     // the url to return all products in category 1 would look like this
@@ -42,32 +65,47 @@ public class CategoriesController
     @GetMapping("{categoryId}/products")
     public List<Product> getProductsById(@PathVariable int categoryId)
     {
+        var category = categoryService.getById(categoryId);
+        if(category == null) return new ArrayList<>();
         // get a list of product by categoryId
-        return null;
+        return productService.listByCategoryId(categoryId);
     }
 
     // add annotation to call this method for a POST action
     // add annotation to ensure that only an ADMIN can call this function
+
+    @PostMapping
+    @PreAuthorize("hasRole('ROLE_ADMIN)")
     public ResponseEntity<Category> addCategory(@RequestBody Category category)
     {
+        var newCategory = categoryService.create(category);
         // insert the category and return it with status 201 Created
-        return null;
+        return ResponseEntity.status(HttpStatus.CREATED).body(newCategory);
     }
 
     // add annotation to call this method for a PUT (update) action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+
+    @PutMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN)")
     public Category updateCategory(@PathVariable int id, @RequestBody Category category)
     {
         // update the category by id and return the updated category (200 OK)
-        return null;
+        return categoryService.update(id, category);
     }
 
 
     // add annotation to call this method for a DELETE action - the url path must include the categoryId
     // add annotation to ensure that only an ADMIN can call this function
+    @DeleteMapping("{id}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ResponseEntity<Void> deleteCategory(@PathVariable int id)
     {
-        // delete the category by id and return status 204 No Content
-        return null;
+
+        if (categoryService.getById(id) == null)
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+
+        categoryService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 }
